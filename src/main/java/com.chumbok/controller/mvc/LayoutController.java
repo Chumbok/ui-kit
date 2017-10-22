5c5c635fd5c09a5fdbb2ef1e2f5e7bb4441bc507
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2017-10-22 21:00:28 +0200

    Move package.

--- src/main/java/com.chumbok/controller/mvc/LayoutController.java
+++ src/main/java/com.chumbok/controller/mvc/LayoutController.java
@@ -1,4 +1,4 @@
-package com.chumbok.controller;
+package com.chumbok.controller.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com.chumbok/controller/rest/InternetConnectionController.java
+++ src/main/java/com.chumbok/controller/rest/InternetConnectionController.java
@@ -1,13 +1,14 @@
-package com.chumbok.controller;
+package com.chumbok.controller.rest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class InternetConnectionController {
 
     @RequestMapping(value = "/connect", method = RequestMethod.GET)
