4159516f74557ba0c76b9e3263b1025b5f84ed37
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-10-21 19:51:02 +0200

    Update prod profile.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,18 +2,11 @@ export const environment = {
   production: true,
   chumbok: {
     enableMock: false,
-
     apiCallThroughLocalServer: true,
-    apiBaseEndpointLocalServer: 'http://localhost:33012',
-
-    /* apiCallThroughGateway: false,
-     apiBaseEndpoint: '//localhost:33002'*/
-
     apiCallThroughGateway: false,
     apiBaseEndpoint: 'http://dev.chumbok.com:33001/gateway',
-
-    pingEndpoint: 'http://localhost:33002/ping',
-    authRefreshEndpoint: 'http://localhost:33002/refresh'
-
+    pingEndpoint: 'http://dev.chumbok.com:33001/gateway/uaa/ping',
+    authRefreshEndpoint: 'http://dev.chumbok.com:33001/gateway/uaa/refresh',
+    apiBaseEndpointLocalServer: 'http://dev.chumbok.com:33012'
   }
 };
