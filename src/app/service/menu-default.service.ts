700ad6b3b89134e65b5ef85800f75ac25e176391
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-10-27 13:35:02 +0100

    Fix default menu service.

--- src/app/service/menu-default.service.ts
+++ src/app/service/menu-default.service.ts
@@ -11,22 +11,22 @@ export class MenuDefaultService implements MenuService {
   constructor() {
 
     const menuItem1: MenuItem = new MenuItem();
-    menuItem1.iconCssClass = 'far fa-building';
-    menuItem1.routerLink = '/menu1/submenu1';
+    menuItem1.iconCssClass = 'fas fa-bug fa-fw';
+    menuItem1.routerLink = '';
     menuItem1.text = 'Submenu 1';
 
     const menu1 = new Menu();
     menu1.id = '#menu1';
     menu1.title = 'Menu 1';
-    menu1.iconCssClass = 'fas fa-user-astronaut fa-fw';
-    menu1.routerLink = '/menu1';
+    menu1.iconCssClass = 'fas fa-brain fa-fw';
+    menu1.routerLink = '';
     menu1.menuItems = [menuItem1];
 
     const menu2 = new Menu();
     menu2.id = '#menu2';
-    menu2.title = 'Menu 1';
-    menu2.iconCssClass = 'fa fa-cog fa-fw';
-    menu2.routerLink = '/menu2';
+    menu2.title = 'Menu 2';
+    menu2.iconCssClass = 'fab fa-canadian-maple-leaf fa-fw';
+    menu2.routerLink = '';
     menu2.menuItems = [];
 
     this.menus = [menu1, menu2];
